6fd29ca8d04d8a1373676a7366d2390fe5382d82
ssttkkl <huang.wen.long@hotmail.com>
2022-07-17 12:05:05 +0800

    支持on_command/on_shell_command/on_startswith/on_endswith/on_keyword Matcher（依赖kmarkdown-it-py）

--- nonebot/adapters/kaiheila/message.py
+++ nonebot/adapters/kaiheila/message.py
@@ -1,12 +1,13 @@
+from dataclasses import dataclass
 from typing import Any, Type, Tuple, Union, Mapping, Iterable, Dict, cast
 
-
-from dataclasses import dataclass
-from nonebot.typing import overrides
+from deprecated import deprecated
+from kmarkdown_it import KMarkdownIt
+from lazy import lazy
 from nonebot.adapters import Message as BaseMessage
 from nonebot.adapters import MessageSegment as BaseMessageSegment
+from nonebot.typing import overrides
 
-from .utils import log, _b2s
 from .exception import UnsupportedMessageType
 
 msg_type_map = {
@@ -19,6 +20,20 @@ msg_type_map = {
     "Card": 10,
 }
 
+# 根据协议消息段类型显示消息段内容
+segment_text = {
+    "text": "[文字]",
+    "image": "[图片]",
+    "video": "[视频]",
+    "file": "[文件]",
+    "audio": "[音频]",
+    "kmarkdown": "[Kmarkdown消息]",
+    "card": "[卡片消息]",
+}
+
+kmd_it = KMarkdownIt()
+
+
 class MessageSegment(BaseMessageSegment["Message"]):
     """
     开黑啦 协议 MessageSegment 适配。具体方法参考协议消息段类型或源码。
@@ -26,47 +41,51 @@ class MessageSegment(BaseMessageSegment["Message"]):
     https://developer.kaiheila.cn/doc/event/message
     """
 
+    # 已知：
+    # command/shell_command使用message_seg = Message[0]; str(message_seg) if message_seg.is_text()
+    # startswith/endswith/keyword使用event.get_plaintext()
+    # regex使用str(Message)
+
     @classmethod
     @overrides(BaseMessageSegment)
     def get_message_class(cls) -> Type["Message"]:
         return Message
 
-    @property
-    def segment_text(self) -> dict:
-        return {
-            "text": "[文字]",
-            "image": "[图片]",
-            "video": "[视频]",
-            "file": "[文件]",
-            "audio": "[音频]",
-            "kmarkdown": "[Kmarkdown消息]",
-            "card": "[卡片消息]",
-        }
-
-    # 根据协议消息段类型显示消息段内容
     def __str__(self) -> str:
-        if self.type in ["text", "KMarkdown", "Card"]:
+        if self.type in ["text", "KMarkdown"]:
             return str(self.data["content"])
         elif self.type == "at":
             return str(f"@{self.data['user_name']}")
         else:
-            return self.segment_text.get(self.type, "")
+            return segment_text.get(self.type, "[未知类型消息]")
+
+    @lazy
+    def plain_text(self):
+        if self.type == "text":
+            return str(self.data["content"])
+        elif self.type == "KMarkdown":
+            return kmd_it.extract_plain_text(self.data["content"])
+        else:
+            return ""
 
     @overrides(BaseMessageSegment)
     def __add__(self, other) -> "Message":
+        # TODO：判断类型
         return Message(self) + (MessageSegment.text(other) if isinstance(
             other, str) else other)
 
     @overrides(BaseMessageSegment)
     def __radd__(self, other) -> "Message":
+        # TODO：判断类型
         return (MessageSegment.text(other)
                 if isinstance(other, str) else Message(other)) + self
 
     @overrides(BaseMessageSegment)
     def is_text(self) -> bool:
-        return self.type == "text"
+        return self.plain_text == self.data["content"]
 
     @staticmethod
+    @deprecated("用 KMarkdown 语法 (met)用户id/here/all(met) 代替")
     def at(user_id: str) -> "MessageSegment":
         return MessageSegment("at", {"user_id": user_id})
 
@@ -91,6 +110,13 @@ class MessageSegment(BaseMessageSegment["Message"]):
         })
 
     @staticmethod
+    def audio(file_key: str, duration: int) -> "MessageSegment":
+        return MessageSegment("audio", {
+            "file_key": file_key,
+            "duration": duration
+        })
+
+    @staticmethod
     def KMarkdown(content: str) -> "MessageSegment":
         return MessageSegment("KMarkdown", {
             "content": content
@@ -102,12 +128,6 @@ class MessageSegment(BaseMessageSegment["Message"]):
             "content": content
         })
 
-    @staticmethod
-    def audio(file_key: str, duration: int) -> "MessageSegment":
-        return MessageSegment("audio", {
-            "file_key": file_key,
-            "duration": duration
-        })
 
 class Message(BaseMessage[MessageSegment]):
     """
@@ -121,12 +141,14 @@ class Message(BaseMessage[MessageSegment]):
 
     @overrides(BaseMessage)
     def __add__(self, other: Union[str, Mapping, Iterable[Mapping]]) -> "Message":
+        # TODO：判断类型
         return super(Message, self).__add__(
             MessageSegment.text(other) if isinstance(other, str) else other
         )
 
     @overrides(BaseMessage)
     def __radd__(self, other: Union[str, Mapping, Iterable[Mapping]]) -> "Message":
+        # TODO：判断类型
         return super(Message, self).__radd__(
             MessageSegment.text(other) if isinstance(other, str) else other
         )
@@ -134,8 +156,9 @@ class Message(BaseMessage[MessageSegment]):
     @staticmethod
     @overrides(BaseMessage)
     def _construct(
-        msg: Union[str, Mapping, Iterable[Mapping]]
+            msg: Union[str, Mapping, Iterable[Mapping]]
     ) -> Iterable[MessageSegment]:
+        # TODO：重构
         if isinstance(msg, Mapping):
             msg = cast(Mapping[str, Any], msg)
             yield MessageSegment(msg["type"], msg.get("content") or {})
@@ -150,7 +173,7 @@ class Message(BaseMessage[MessageSegment]):
 
     @overrides(BaseMessage)
     def extract_plain_text(self) -> str:
-        return "".join(seg.data["content"] for seg in self if seg.is_text())
+        return "".join(seg.plain_text for seg in self)
 
 
 @dataclass
@@ -165,7 +188,7 @@ class MessageSerializer:
     async def serialize(self) -> Tuple[str, str]:
         if self.message[0].type in ("text", "KMarkdown", "Card"):
             return msg_type_map[self.message[0].type], self.message[0].data['content']
-        elif self.message[0].type in  ("image", "audio", "video", "file"):
+        elif self.message[0].type in ("image", "audio", "video", "file"):
             return self.message[0].data['file_key']
         else:
             raise UnsupportedMessageType
@@ -177,20 +200,20 @@ class MessageDeserializer:
     开黑啦 协议 Message 反序列化器。
     """
     type: str
-    datas: Dict
+    data: Dict
 
     def deserialize(self) -> Message:
         if self.type == 1:
-            return Message(MessageSegment.text(self.datas["content"]))
+            return Message(MessageSegment.text(self.data["content"]))
         elif self.type == 2:
-            return Message(MessageSegment.image(self.datas["content"]))
+            return Message(MessageSegment.image(self.data["content"]))
         elif self.type == 3:
-            return Message(MessageSegment.video(self.datas["attachments"]["url"]))
+            return Message(MessageSegment.video(self.data["attachments"]["url"]))
         elif self.type == 4:
-            return Message(MessageSegment.file(self.datas["attachments"]["url"]))
+            return Message(MessageSegment.file(self.data["attachments"]["url"]))
         elif self.type == 9:
-            return Message(MessageSegment.KMarkdown(self.datas["content"]))
+            return Message(MessageSegment.KMarkdown(self.data["content"]))
         elif self.type == 10:
-            return Message(MessageSegment.Card(self.datas["content"]))
+            return Message(MessageSegment.Card(self.data["content"]))
         else:
             return Message(MessageSegment(self.type, self.data))
