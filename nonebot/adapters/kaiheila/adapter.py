947b54f10b82a1cd6b131cca2d95ea724668df03
lclbm <614867321@qq.com>
2024-02-14 09:29:54 +0800

    [feat] 忽略处理其他bot的事件消息

--- nonebot/adapters/kaiheila/adapter.py
+++ nonebot/adapters/kaiheila/adapter.py
@@ -394,8 +394,8 @@ class Adapter(BaseAdapter):
             # 不存在的signal，resume是不可能resume的，这辈子都不会resume的，出了问题直接重连
             return
 
-        # 屏蔽 Bot 自身的消息
-        if json_data["d"].get("author_id") == self_id:
+        # 屏蔽 Bot 自身和其他 Bot 的消息
+        if json_data["d"].get("author_id") == self_id or json_data['d'].get('extra', {}).get('author', {}).get('bot'):
             return
 
         try:
